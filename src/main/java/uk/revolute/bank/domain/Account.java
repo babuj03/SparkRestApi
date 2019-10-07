40c65e53b718f1eb21671b9cdc1a103dc58c3690
babuj03 <babuj03@gmail.com>
2019-10-07 21:44:25 +0300

    Update Account.java
    added accountholder Id field

--- src/main/java/uk/revolute/bank/domain/Account.java
+++ src/main/java/uk/revolute/bank/domain/Account.java
@@ -1,23 +1,25 @@
 package uk.revolute.bank.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 /**
  * @author JBabu
  * @Created Date : 04/10/2019
  */
 @Entity
+@Table(uniqueConstraints={
+        @UniqueConstraint(columnNames = {"accountHolderId"})
+})
 public class Account extends  BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountNumber;
+    private String accountHolderId;
     private String accountHolderName;
     private LocalDate accountHolderDOB;
     private double currentBalace;
+    private String baseCurrency ="GBP";
     private String branch;
 
     public double getCurrentBalace() {
@@ -62,11 +64,27 @@ public class Account extends  BaseEntity{
         this.accountHolderDOB = accountHolderDOB;
     }
 
+    public String getAccountHolderId() {
+        return accountHolderId;
+    }
+
+    public void setAccountHolderId(String accountHolderId) {
+        this.accountHolderId = accountHolderId;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
 
     @Override
     public String toString() {
         return "Account{" +
                 "accountNumber=" + accountNumber +
+                ", accountHolderName='" + accountHolderName + '\'' +
                 ", accountHolderName='" + accountHolderName + '\'' +
                 ", accountHolderDOB=" + accountHolderDOB +
                 ", currentBalace=" + currentBalace +
